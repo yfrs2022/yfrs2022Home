8319730af2e46cfdb001080357f8c14bf314a865
yfrs2022 <yfrs2022@163.com>
2022-10-02 09:58:08 +0800

    init commit

---
+++ home/src/test/java/com/yfrs2022/home/HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yfrs2022.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
